94235e009e0ef7c07648b2f126b555019b6af642
pseudo <pseudo@pseudo.moi>
2018-09-04 11:23:32 -0400

    add login + session

--- crud.py
+++ crud.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from model import User, UserSchema, RoleSchema, Session
 from passlib.hash import argon2
 from marshmallow import pprint
 
+import os
+import json
+
 
 #from wgsi import app
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-session = Session()
+db_session = Session()
 
 user_schema = UserSchema(only=('id', 'login', 'role', 'mail'))
 users_schema = UserSchema(only=('id', 'login', 'role', 'mail'), many=True)
@@ -20,24 +24,25 @@ def hello_world():
 
 @app.route("/users", methods=["POST"])
 def add_user():
-  if not all (k in request.json for k in ('login', 'password', 'id_role', 'mail')):
-    return "Missing info : should furnish login, password, id_role"
+  expected = ('login', 'password', 'id_role', 'mail')
+  if not all (k in request.json for k in expected):
+    return "Missing info : should furnish " + " ".join(expected)
 
   login = request.json['login']
   mail = request.json['mail']
   password = argon2.hash(request.json['password'])
   id_role = request.json['id_role']
 
-  user = session.query(User).filter(User.login == login).all()
+  user = db_session.query(User).filter(User.login == login).all()
 
   if len(user) != 0:
     return "Login already taken"
 
   new_user = User(login, password, id_role, mail)
 
-  session.add(new_user)
-  session.commit()
-  session.refresh(new_user)
+  db_session.add(new_user)
+  db_session.commit()
+  db_session.refresh(new_user)
 
   epure = user_schema.dumps(new_user)
   return jsonify(epure.data)
@@ -45,14 +50,14 @@ def add_user():
 
 @app.route("/users", methods=["GET"])
 def get_user():
-  all_users = session.query(User).all()
+  all_users = db_session.query(User).all()
   result = users_schema.dumps(all_users)
   return jsonify(result.data)
 
 
 @app.route("/users/<login>", methods=["GET"])
 def user_detail(login):
-  user = session.query(User).filter(User.login == login).all()
+  user = db_session.query(User).filter(User.login == login).all()
 
   print len(user)
   try:
@@ -61,6 +66,30 @@ def user_detail(login):
     return jsonify(user_schema.dump(user[0]).data)
   except ValueError as err:
     return err
+
+@app.route('/login', methods=['POST'])
+def login():
+  expected = ('login', 'password')
+  if not all (k in request.json for k in expected):
+    return "Missing info : should furnish " + " ".join(expected)
+
+
+  login = request.json['login']
+  password = request.json['password']
+
+  if login in session:
+    return 'Already logged in !'
+  if login in session:
+    return 'Already initiated : {}'.format(session[login])
+
+  user = db_session.query(User).filter(User.login == login).first()
+  if argon2.verify(password, user.password) == True:
+    print json.dumps({ 'id': user.id, 'login': user.login, 'role': user.role.name })
+    session[login] = json.dumps({ 'id': user.id, 'login': user.login, 'role': user.role.name })
+    return 'Session initiated {}'.format(session[login])
+  else:
+    return 'No session'
+
 
 #@app.route("/users/<username>", methods=["PUT", "PATCH"])
 #def user_update(username):
@@ -80,7 +109,7 @@ def user_detail(login):
 #  if username:
 #    user.username = username
 #
-#  session.commit()
+#  db_session.commit()
 #  return user_schema.jsonify(user)
 #
 #@app.route("/users/<username>", methods=["DELETE"])
@@ -91,8 +120,8 @@ def user_detail(login):
 #    return User.handle_error_db(user)
 #  user = user[0]
 #
-#  session.delete(user)
-#  session.commit()
+#  db_session.delete(user)
+#  db_session.commit()
 #
 #  return user_schema.jsonify(user)
 #
